770817e9394e41e5ecce683885b92c0e7e2176f8
esandrea01 <77824799+esandrea01@users.noreply.github.com>
2021-03-02 01:40:53 -0500

    Add files via upload

--- changeImage.py
+++ changeImage.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
- 
-import os
 from PIL import Image
- 
-path = '/home/erick/images/'
- 
-for image in os.listdir(path):
-	if '.png' in image and '.' not in image[0]:
-		img = Image.open(path + image)
-		img.resize((600, 400)).convert("RGB").save(path + image.split('.')[0] + '.jpeg' , 'jpeg')
-		img.close()
+import os
+
+arr = os.listdir()
+for file in arr:
+ if file.endswith('tiff'):
+  im = Image.open(file)
+  new_im = im.resize((600,400))
+  new_im.convert('RGB').save('/supplier-data/images'+ file +'.jpeg')
